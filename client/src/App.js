849fdabec0621bd5840267f2ba93ad5d5a6d57f8
MrLucasAlmeida <lucasoa2701@live.com>
2023-01-17 11:43:18 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import SearchIcon from './search.svg'
+import { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
+
+
+
+const App = () => {
+
+  
+  const [movies,setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const movieSearch = async (title) => {
+    const response = await fetch('http://localhost:5000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: title
+    })
+  });
+    if (response.ok) {
+      const data = await response.json();
+      // returns if the data is empty
+      if (data.movies === undefined) {
+        return;
+      }
+      setMovies(data.movies);
+      
+    } else {
+      const err = await response.json();
+      console.log(err);
+      console.log("ERROR");
+      alert(err);
+    }
+    
+
+  }
+
+  useEffect(() => {
+    movieSearch(searchTerm);
+  }, []);
+
+  useEffect(() => {
+    movieSearch(searchTerm);
+  }, [searchTerm])
+
+
+
+
+
+
+  return (
+    <div className="app">
+      <h1>MovieSearch</h1>
+
+      <div className="search">
+        <input
+          placeholder='Search All Movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='search icon'
+          onClick={() => movieSearch(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length > 0 ? (
+          <div className='container'>
+            {
+            movies.map((movie) =>  (
+              <MovieCard movie={movie}/>
+            ))}
+            
+          </div>
+        ) : (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as doteven from 'dotenv';
+import cors from 'cors';
+
+doteven.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const URL = 'http://www.omdbapi.com?apikey=' + process.env.OMDB_API_KEY;
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Hello Movie Search User'
+    });
+});
+
+app.post('/', async (req, res) => {
+    try {
+        // const moviesList = await fetch(`${URL}&s=${req.body.query}`);
+        const moviesList = await fetch(`${URL}&s=${req.body.query}`);
+        const data = await moviesList.json();
+        const search = data.Search;
+        res.status(200).send({
+            movies: search
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+
+    }
+});
+
+
+app.listen(5000, () => {console.log('Server is running on port http://localhost:5000')});
