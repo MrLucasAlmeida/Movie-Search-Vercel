13a2be17d13f3f282e63c2b60336ed487589abd3
MrLucasAlmeida <lucasoa2701@live.com>
2023-03-08 22:07:24 -0800

    added cors header

--- server/server.js
+++ server/server.js
@@ -11,6 +11,16 @@ app.use(express.json());
 const URL = 'http://www.omdbapi.com?apikey=' + process.env.OMDB_API_KEY;
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+
 
 app.get('/', (req, res) => {
     res.status(200).send({
