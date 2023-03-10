4aad5d17d2967ea477239fb0b6ca90d8b50209ee
MrLucasAlmeida <lucasoa2701@live.com>
2023-03-09 21:29:08 -0800

    updated origin

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-// app.use(cors());
+
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://movie-search-app-frontend.onrender.com"]
+}));
 
 const URL = 'http://www.omdbapi.com?apikey=' + process.env.OMDB_API_KEY;
 const port = process.env.PORT || 5000;
